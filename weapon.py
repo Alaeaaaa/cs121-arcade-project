b4be99cb86bafa54a88c897b2bfb9a2045048830
Alae Belkhadir <alae.belkhadir@epfl.ch>
2026-05-27 18:51:01 +0200

    dé

--- weapon.py
+++ weapon.py
@@ -8,7 +8,7 @@ from direction import Direction
 class Weapon(arcade.TextureAnimationSprite):
 
 
-    def __init__(self, animation, scale: float):
+    def __init__(self, animation: arcade.TextureAnimation , scale: float):
 
         super().__init__(animation=animation, scale=scale)
         self.direction = Direction.SOUTH
