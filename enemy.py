d525c5b7ddf2d9eb39aeadd13a9bfda8b9346653
Alae Belkhadir <alae.belkhadir@epfl.ch>
2026-05-27 14:10:10 +0200

    a

--- enemy.py
+++ enemy.py
@@ -1,14 +1,11 @@
-<<<<<<< HEAD
 from dataclasses import dataclass
 from abc import abstractmethod
-=======
 from abc import abstractmethod
 
->>>>>>> 7607e3b (c)
 import arcade
 from navmesh import NavMesh, Point
 import random
-@dataclass
+@dataclas
 class EnemyContext:
     """cette classe est vraiment juste utile pour la fonction update,
     ça nous permet de garder update comme méthode abstraite sans passer par **kwargs
@@ -20,22 +17,22 @@ class EnemyContext:
     player_position: Point
     walls: arcade.SpriteList
 
-<<<<<<< HEAD
+
 class Enemy(arcade.TextureAnimationSprite):
     """classe commune à tous nos ennemis. On hérite de textureanimationsprite
     car tous les ennemis ont une position, animation, échelle...
     les ennemis ont également tous une logique qu'il faut mettre à jour : update_logic()
     et il faut lier l'aspect logique au visuel : sync_sprite() """
-=======
+
 
 class Enemy(arcade.TextureAnimationSprite):
->>>>>>> 7607e3b (c)
+
 
     def __init__(self, animation: arcade.TextureAnimation, scale: float) -> None:
         super().__init__(animation=animation, scale=scale)
 
     @abstractmethod
-<<<<<<< HEAD
+
     def update_logic(self, context:EnemyContext) -> None:
         """après je ne sais pas combien de refactoring, on souffle enfin.
         on s'est débarassé des **kwargs en introduisant la nouvelle classe EnemyContext.
@@ -45,10 +42,10 @@ class Enemy(arcade.TextureAnimationSprite):
     def sync_sprite(self) -> None:
         """synchronise simplement la position du sprite en accord avec la logique"""
         ...
-=======
+
     def update_logic(self, **kwargs) -> None:
         ...
 
     def sync_sprite(self) -> None:
         ...
->>>>>>> 7607e3b (c)
+
